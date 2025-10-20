f3ba0e71f51370ec5290a32a32a740fe99059081
Erik Jones <ejones99@umd.edu>
2025-10-20 11:26:32 -0400

    Revert "LIBILS-1012. Correct Journal Auto-Installation."
    This reverts commit a7f5200d37657844f260e1770ad7fbf20e5d66ea.

--- extract_press_netloc.py
+++
@@ -1,6 +0,0 @@
-import os
-from urllib.parse import urlparse
-
-if __name__ == "__main__":
-    janeway_press_domain = os.getenv("JANEWAY_PRESS_URL", "")
-    print(urlparse(janeway_press_domain).netloc)
--- prod_settings.py
+++ prod_settings.py
@@ -1,5 +1,4 @@
 import os
-from urllib.parse import urlparse
 from .janeway_global_settings import LOGGING # type: ignore
 
 # Set the static and media directories 
@@ -14,41 +13,34 @@ DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() in ("true", "1", "yes")
 if os.getenv("INSTALLATION_BASE_THEME"):
     INSTALLATION_BASE_THEME = os.getenv("INSTALLATION_BASE_THEME")
 
-# ------------------------------- NETWORKING -------------------------------
-
-press_domain = os.environ.get('JANEWAY_PRESS_URL')
-parsed_press_domain = urlparse(press_domain) # Get Netloc from press domain
 ALLOWED_HOSTS = [
-    parsed_press_domain.netloc
+    os.environ.get('JANEWAY_PRESS_DOMAIN')
 ]
 
-CSRF_TRUSTED_ORIGINS=[
-    press_domain
-]
-
-# Add local values if DEBUG is active.
 if DEBUG:
     ALLOWED_HOSTS.append("127.0.0.1")
     ALLOWED_HOSTS.append("localhost")
     ALLOWED_HOSTS.append("janeway")
+
+journal_domains = os.getenv("JANEWAY_JOURNAL_DOMAINS", "").split(",")
+if journal_domains:
+    for domain in journal_domains:
+        ALLOWED_HOSTS.append(domain)
+
+CSRF_TRUSTED_ORIGINS=[
+    os.environ.get('JANEWAY_PRESS_DOMAIN_SCHEME') + os.environ.get('JANEWAY_PRESS_DOMAIN')
+]
+
+if DEBUG:
     CSRF_TRUSTED_ORIGINS.append("http://127.0.0.1")
     CSRF_TRUSTED_ORIGINS.append("http://localhost")
     CSRF_TRUSTED_ORIGINS.append("http://janeway")
 
-journal_domains = os.getenv("JANEWAY_JOURNAL_URLS", "").split(",")
+journal_domains = os.getenv("JANEWAY_JOURNAL_DOMAINS", "").split(",")
+journal_domain_schemes = os.getenv("JANEWAY_JOURNAL_DOMAIN_SCHEMES", "").split(",")
 if journal_domains:
-    for domain in journal_domains:
-        # Parse out components of domain
-        parsed_domain = urlparse(domain)
-
-        # Add the netloc to allowed hosts
-        if parsed_domain.netloc not in ALLOWED_HOSTS:
-            ALLOWED_HOSTS.append(parsed_domain.netloc)
-
-        # Add the full domain to the CSRF_TRUSTED_ORIGINS
-        if domain not in CSRF_TRUSTED_ORIGINS:
-            CSRF_TRUSTED_ORIGINS.append(domain)
-
-# ------------------------------- LOGGING -------------------------------
+    for i, domain in enumerate(journal_domains):
+        if domain:
+            CSRF_TRUSTED_ORIGINS.append(journal_domain_schemes[i] + journal_domains[i])
 
 LOGGING['handlers']['log_file']['filename'] = "/var/www/janeway/logs/janeway.log"
